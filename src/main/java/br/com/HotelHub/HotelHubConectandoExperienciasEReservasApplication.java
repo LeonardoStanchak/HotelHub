ea2e92d3849098c7a64bfe9e4e3a09cc77f8630e
Leonardo Stanchak <leonardostanchak@hotmail.com>
2024-01-18 19:22:32 -0300

    feat: start project

---
+++ src/main/java/br/com/HotelHub/HotelHubConectandoExperienciasEReservasApplication.java
@@ -0,0 +1,13 @@
+package br.com.HotelHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelHubConectandoExperienciasEReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelHubConectandoExperienciasEReservasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/HotelHub/HotelHubConectandoExperienciasEReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.HotelHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelHubConectandoExperienciasEReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
